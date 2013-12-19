9426d3ebaeb65b89dd5fced08a0197d1213c688f
Omer's minilaptop <omer.tzuk@student.uva.nl>
2013-12-19 10:22:03 +0100

    Second commit

---
+++ wimp_cmbdistortions.hh
@@ -0,0 +1,24 @@
+#ifndef WIMP_CMB_DISTORTIONS
+#define WIMP_CMB_DISTORTIONS
+
+class wimp {
+	
+	
+private :
+	double mass ;
+	double sigma_v ;
+	double z_annihilation ;
+	
+} ;
+
+class mu_distortion {
+	
+} ;
+
+class y_distortion {
+	
+} ;
+
+
+
+#endif
