f29a55ed496a2054e8d0475cfd5642b24bbacdd8
Omer's minilaptop <omer.tzuk@student.uva.nl>
2014-01-22 15:17:43 +0100

    mssm_data_Reader works in extracting columns

--- data_mining.py
+++ data_mining.py
@@ -1,13 +1,17 @@
 import PPPC4DMID_Reader as pppc
+import mssm_data_Reader as mssm
 
+mssm = mssm.Read_pp_log_mssm_data("data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR")
 
-final_SM_nu_e = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_e.dat")
-final_SM_nu_mu = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_mu.dat")
-final_SM_nu_tau = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_tau.dat")
-final_SM_positrons = pppc.ReadPPPC4DMID_data("data/AtProduction_positrons.dat")
-final_SM_gammas = pppc.ReadPPPC4DMID_data("data/AtProduction_gammas.dat")
-final_SM_antiprotons = pppc.ReadPPPC4DMID_data("data/AtProduction_antiprotons.dat")
-final_SM_antideuterons = pppc.ReadPPPC4DMID_data("data/AtProduction_antideuterons.dat")
+#print mssm.data["m_1 (GeV)"][0:10]
+
+#final_SM_nu_e = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_e.dat")
+#final_SM_nu_mu = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_mu.dat")
+#final_SM_nu_tau = pppc.ReadPPPC4DMID_data("data/AtProduction_neutrinos_tau.dat")
+#final_SM_positrons = pppc.ReadPPPC4DMID_data("data/AtProduction_positrons.dat")
+#final_SM_gammas = pppc.ReadPPPC4DMID_data("data/AtProduction_gammas.dat")
+#final_SM_antiprotons = pppc.ReadPPPC4DMID_data("data/AtProduction_antiprotons.dat")
+#final_SM_antideuterons = pppc.ReadPPPC4DMID_data("data/AtProduction_antideuterons.dat")
 
 #e_int = nu_e.interp_integrated_column('e')
 		
--- mssm_data_Reader.py
+++ mssm_data_Reader.py
@@ -3,16 +3,48 @@ import scipy.integrate
 from scipy.interpolate import interp1d
 
 
-
-#info_file = "data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.info"
-
-#with open(info_file,'r') as definitions:
-			#definitions = definitions.readlines()
-			##definitions = definitions.split()
-			#print definitions
-			
-data_file = "data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.txt"
-
-data_table = np.loadtxt(data_file, skiprows=0 , unpack=True)
-
-print data_table[0][10:]
+class Read_pp_log_mssm_data(object):
+	'''
+	This class is built to handle data from the files 
+	data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.info
+	data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.txt
+	'''
+	def __init__(self, filename):
+		self.load_info(filename)
+		self.load_data(filename)
+		
+	def load_info(self, filename):			
+		info_file = filename+".info"
+		#"data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.info"
+		
+		with open(info_file,'r') as lines:
+					lines = lines.readlines()
+					#definitions = definitions.split()
+					#index_labs = lines.index("lab1")
+					#print index_lab
+					self.columns_defs = {}
+					for line in lines:
+						if line.find("lab") == 0:
+							zero_index = line.find("=")
+							self.columns_defs[int(line[3:zero_index])+2] = line[zero_index+2:-1]
+					#labs = [s for s in lines if "lab" in s]
+					#print self.columns_defs
+					
+	
+	def load_data(self, filename):
+		txt_file = filename+".txt"
+		#data_file = "data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.txt"
+		table = np.loadtxt(txt_file, skiprows=0 , unpack=True)
+		
+		#print len(table)
+		#print len(table[0])
+		
+		self.data = {}
+		
+		self.data["multip"] = table[0]
+		self.data["chisq"] = table[1]
+		
+		for key in self.columns_defs:
+			#print key, self.columns_defs[key]
+			self.data[self.columns_defs[key]] = table[key-1]
+		
