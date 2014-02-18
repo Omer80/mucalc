cf4f3e8f07d61778864197a51409756d02135f66
Omer80 <omer.tzuk@student.uva.nl>
2014-02-18 21:37:48 +0100

    Working calc_f_gamma version 0.1 with HDF5 file saving

--- calc_f_gamma.py
+++ calc_f_gamma.py
@@ -92,56 +92,17 @@ def calc_f_gamma(row, nu_fractions):
 	return (1. - f_nu)
 	
 		
-def main(channels = ['eL'], mass = 200, save_figures = False, detailed_plots = False):
+def main():
 	mssm_data = load_mssm_data()
 	table = produce_table(mssm_data,['Z','W','t','b','\[Gamma]'])
 	#print table
-	table.write("table.hdf5", format='hdf5',path='data')	
-
-	
-	
-	
+	table.write("data/pp_log_mssm1_cdm_mup_CTA_sigmavXBR.hdf5", format='hdf5',path='data')	
 
 
 # Parser setup
 parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('-c','--channels', nargs='+',
-                   help='Choose specific channel for plotting')
-                   
-parser.add_argument('-a','--all_channels', help="Producing pie charts for all channels",
-					action='store_true')
-parser.add_argument('-d','--detailed_plots', help="Producing pie charts",
-					action='store_true')
-parser.add_argument('-s','--save_figures', help="Saves the pie charts to files",
-					action='store_true')
-parser.add_argument('-m','--mass', help="Specifing the mass for the calculations",
-					 action='count', default = 200)
-parser.add_argument('-p','--print_possible_channels', help="Prints all possible channels for calculation, exit",
-					action='store_true')
 
 args = parser.parse_args()	
 if __name__ == "__main__":
-	if args.print_possible_channels:
-		with open("data/AtProduction_neutrinos_e.dat",'r') as definitions:
-			definitions = definitions.readline()
-			definitions = definitions.split()
-			print definitions
-			
-	else:	
-		if args.all_channels:
-			with open("data/AtProduction_neutrinos_e.dat",'r') as definitions:
-				definitions = definitions.readline()
-				definitions = definitions.split()
-			channels = definitions[2:]
-		else:
-			if args.channels:
-				channels = args.channels
-			else:
-				channels = ['eL']
-		
-		if args.mass != None:
-			mass = int(args.mass)
-		else:
-			mass = 200	
-		main(channels, mass, args.save_figures, args.detailed_plots)
+	main()
 	
