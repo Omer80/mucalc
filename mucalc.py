ba384f652e63c7bb44a71aa3c717963aaef1faae
Omer's minilaptop <omer.tzuk@student.uva.nl>
2014-02-19 14:41:16 +0100

    Adding plotting to mucalc

--- mucalc.py
+++ mucalc.py
@@ -12,6 +12,12 @@ from scipy import integrate
 from scipy.interpolate import interp1d
 import const
 import math
+import argparse
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+## for Palatino and other serif fonts use:
+#rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
 
 cosmology.core.set_current(cosmology.Planck13)
 
@@ -80,6 +86,8 @@ def wimp_mucalc(mDM, sigma_v, f_gamma):
 	return mu_0(z_i, z_min,f_gamma, mDM, sigma_v)
 	
 
+H_z = lambda z : cosmology.H(z).to(1/u.s)
+
 def ndm_z(z):
 	"""
 	"""
@@ -89,6 +97,7 @@ def dQdz(f_gamma, mDM, sigma_v, z):
 	"""
 	Energy injection calculation from eq. (5.5) in arXiv: 1203.2601v2 
 	"""
+	mDM = (mDM * const.GeV)/(const.c**2)
 	dQdz_numerator = (mDM * const.c**2 * (ndm_z(z)**2) * sigma_v)
 	dQdz_denominator = (const.a * ((const.TCMB * (1+z))**4))
 	return f_gamma * (dQdz_numerator/dQdz_denominator)
@@ -135,11 +144,45 @@ def mu_0(z_i, z_min,f_gamma, mDM, sigma_v):
 	I do not consider any initial mu, so the first part of the equation is not taken into account
 	"""
 	#first_part = mu(z_i) * np.math.exp(-tau(z_i))
-	H_z = lambda z : cosmology.H(z).to(1/u.s)
+	
 	# With dN/dz
 	#integrand = lambda z: ((1/((1+z)*H_z(z)))* (dQdz(z)-(4/3)*(dNdz(z))) * math.exp(-tau(z))) 
 	# Without dN/dz
 	integrand = lambda z: ((1/((1+z)*H_z(z)))* (dQdz(z)) * math.exp(-tau(z)))
 	second_part = const.C*const.B*integrate.romberg(integrand,z_min,z_i)
 	#return first_part+second_part
-	return second_part 
+	return second_part
+	
+
+def main():
+	sigma_v = 3e-27 / (const.Omega_cdm * (const.h0**2)) 
+	dot_epsilon = lambda z: dQdz(1,10,sigma_v, z)
+	y = lambda z: dot_epsilon(z) * (math.exp(-tau(z))/H_z(z).value)
+	print "sigma_v",sigma_v
+	print "dot_epsilon", dot_epsilon(10**5)
+	print "H", H_z(10**5)
+	print "tau",tau(10**5)
+	print "y axis of Figure 10 for z= 10^5 is :  ", y(10**5)
+	plot(y,100, 5e6)
+
+def plot(function, min_x, max_x):
+	t = np.logspace(min_x, max_x, 1000)
+	s = function(t)
+	plt.plot(t, s, 'b-', lw=2)
+	
+	plt.xlabel(r'z')
+	plt.ylabel(r'(1+z)G d\eps / dz')
+	plt.title(r'Energy injection from dark matter annihilation for 10 GeV WIMP with $f_{\gamma} = 1$')
+	plt.grid(True)
+	plt.show()
+
+	
+
+
+
+# Parser setup
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+args = parser.parse_args()	
+if __name__ == "__main__":
+	main()
